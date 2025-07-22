97925a4cc10aaaee5bd92812ff6f38c2448db303
Prathyusha Nagavalli <Prathyusha.Nagavalli@gmail.com>
2025-07-21 19:49:51 -0500

    Create even_odd.py

---
+++ python-scripts/even_odd.py
@@ -0,0 +1,10 @@
+# Check if a number is even or odd
+
+def check_even_odd(number):
+    if number % 2 == 0:
+        print(f"{number} is Even")
+    else:
+        print(f"{number} is Odd")
+
+# Example: Check 7
+check_even_odd(7)
